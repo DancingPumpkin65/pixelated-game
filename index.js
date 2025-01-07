d8f7b52cb0d90741bde6061dae0b495ebd45c407
Scripted Logic <ouassbader184@gmail.com>
2025-01-07 13:27:02 +0100

    Added background sprite

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const c = canvas.getContext('2d')
 canvas.width = 64 * 16  // 1024
 canvas.height = 64 * 9  // 576
 
+const backgroundLevel1 = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/backgroundLevel1.png'
+})
+
 // Create a character + animate
 const player = new Player()
 
@@ -19,48 +27,19 @@ const keys = {
         pressed: false
     }
 }
+
 function animate() {
     window.requestAnimationFrame(animate)
-    c.fillStyle = 'white'
-    c.fillRect(0, 0, canvas.width, canvas.height) 
+
+    backgroundLevel1.draw()
 
     player.velocity.x = 0
-    if (keys.d.pressed) {
-        player.velocity.x = 5
-    } else if (keys.a.pressed) {
-        player.velocity.x = -5
-    }
+    if (keys.d.pressed) player.velocity.x = 5
+    else if (keys.a.pressed) player.velocity.x = -5
+    
     player.draw()
     player.update()
 }
 
 animate()
 
-// Adding Gravity
-window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'w':
-            if (player.velocity.y === 0) player.velocity.y = -20
-            
-            break
-        case 'a':
-            keys.a.pressed = true
-            break
-        case 'd':
-            keys.d.pressed = true
-            break
-    }
-})
-
-window.addEventListener('keyup', (event) => {
-    switch (event.key) {
-        case 'a':
-            keys.a.pressed = false
-            
-            break
-        case 'd':
-            keys.d.pressed = false
-            
-            break
-    }
-})
