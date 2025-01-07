1e6c3f14fc5ff897e9158cfef180f39713452a3e
Scripted Logic <ouassbader184@gmail.com>
2025-01-07 11:50:22 +0100

    First commit to repo

---
+++ index.js
@@ -0,0 +1,20 @@
+// Setup dimensions
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 64 * 16  // 1024
+canvas.height = 64 * 9  // 576
+
+// Create a character + animate
+const player = new Player()
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height) 
+
+    player.draw()
+    player.update()
+}
+
+animate()
