5ae2d347fe2a1c179ef64703deb0184816d68626
Scripted Logic <ouassbader184@gmail.com>
2025-01-07 13:07:40 +0100

    Adding gravity and mouvement to player

--- index.js
+++ index.js
@@ -8,13 +8,59 @@ canvas.height = 64 * 9  // 576
 // Create a character + animate
 const player = new Player()
 
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height) 
 
+    player.velocity.x = 0
+    if (keys.d.pressed) {
+        player.velocity.x = 5
+    } else if (keys.a.pressed) {
+        player.velocity.x = -5
+    }
     player.draw()
     player.update()
 }
 
 animate()
+
+// Adding Gravity
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'w':
+            if (player.velocity.y === 0) player.velocity.y = -20
+            
+            break
+        case 'a':
+            keys.a.pressed = true
+            break
+        case 'd':
+            keys.d.pressed = true
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'a':
+            keys.a.pressed = false
+            
+            break
+        case 'd':
+            keys.d.pressed = false
+            
+            break
+    }
+})
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -4,6 +4,11 @@ class Player {
             x: 100,
             y: 100
         }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.gravity = 1
         this.width = 100
         this.height = 100
         this.sides = {
@@ -17,9 +22,13 @@ class Player {
     }
 
     update() {
-        if (this.sides.bottom < canvas.height) {
-            this.position.y++
-            this.sides.bottom = this.position.y + this.height
-        }
+        this.position.x += this.velocity.x 
+        this.position.y += this.velocity.y // Gravity
+        this.sides.bottom = this.position.y + this.height
+
+        // Above bottom of canvas 
+        if (this.sides.bottom + this.velocity.y < canvas.height) {
+            this.velocity.y += this.gravity
+        } else this.velocity.y = 0
     }
 }
